bf35588f7be9fc6510469052602c6f012bbaed7e
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-23 12:15:28 +0000

    Hero Section Completed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,16 @@
+import heroGrid from "../assets/images/hero-grid.png";
+
 export default function Hero() {
   return (
-    <div className="Hero">
+    <div className="hero">
+      <div className="hero__image-wrapper">
+        <img src={heroGrid} alt="" />
+      </div>
+      <h1>Online Experiences</h1>
+      <p>
+        Join unique interactive activities led by one-of-a-kind hosts—all
+        without leaving home.
+      </p>
     </div>
   );
 }
